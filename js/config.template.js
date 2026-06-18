ecc0c7416913a32d3fac7c1704e7189b417a4d4b
MrChilaquiles <RGGA2000@gmail.com>
2026-06-18 12:59:05 -0600

    feat: Add application configuration template for backend URL and environment settings

---
+++ js/config.template.js
@@ -0,0 +1,5 @@
+window.APP_CONFIG = {
+  BACKEND_URL: "${BACKEND_URL}",
+  WEBHOOK_URL: "https://n8nmrb.marblock.shop/webhook/5f3d84df-0d66-4ea8-a9dd-285f7e6f7dc0",
+  ENVIRONMENT: "${ENVIRONMENT}"
+};
