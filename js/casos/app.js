b7fdcbff43d434f5cda91cef9d8fe5107ad867a6
MrChilaquiles <RGGA2000@gmail.com>
2026-09-15 12:10:46 -0600

    fix(casos): cuadrar columnas de la tabla de gerente/admin
    Ajusta el colgroup de Por aprobar/Historial y evita que el folio/texto desborde su celda (overflow + ellipsis) y que los badges se salgan.

--- js/casos/app.js
+++ js/casos/app.js
@@ -1022,8 +1022,8 @@ function renderRevisionView() {
       <div class="table-wrap">
         <table class="casos-table">
           <colgroup>
-            <col style="width:12%" /><col style="width:12%" /><col style="width:12%" /><col style="width:9%" />
-            <col style="width:19%" /><col style="width:15%" /><col style="width:13%" /><col style="width:8%" />
+            <col style="width:13%" /><col style="width:12%" /><col style="width:12%" /><col style="width:7%" />
+            <col style="width:19%" /><col style="width:15%" /><col style="width:14%" /><col style="width:8%" />
           </colgroup>
           <thead>
             <tr>
